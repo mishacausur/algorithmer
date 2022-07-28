c46596ac770cab2e355039131984c758b755cbd3
Misha Causur <mishacausur@mail.ru>
2022-07-28 21:37:59 +0500

    stack data structure is implemented

---
+++ algorithmer/stack.swift
@@ -0,0 +1,30 @@
+//
+//  stack.swift
+//  algorithmer
+//
+//  Created by Misha Causur on 28.07.2022.
+//
+
+import Foundation
+
+public struct Stack<Element> {
+    
+    private var storage: [Element] = .init()
+    
+    public init() {}
+    
+    public mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+}
+
+extension Stack: CustomDebugStringConvertible {
+    
+    public var debugDescription: String {
+        """
+            --- TOP ---
+            \(storage.map { " \($0) " }.reversed().joined(separator: "\n"))
+            --- END ---
+        """
+    }
+}
