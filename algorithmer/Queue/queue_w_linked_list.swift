0829f2ae790b2c9ebee20208f1a82546f7472480
Misha Causur <mishacausur@mail.ru>
2022-08-03 00:23:29 +0500

    queue double linked list still being in progress

--- algorithmer/Queue/queue_w_linked_list.swift
+++ algorithmer/Queue/queue_w_linked_list.swift
@@ -7,6 +7,92 @@
 
 import Foundation
 
+public class DoublyLinkedList<T> {
+  
+  private var head: DNode<T>?
+  private var tail: DNode<T>?
+  
+  public init() { }
+  
+  public var isEmpty: Bool {
+    head == nil
+  }
+  
+  public var first: DNode<T>? {
+    head
+  }
+  
+  public func append(_ value: T) {
+    let newNode = DNode(value: value)
+    
+    guard let tailNode = tail else {
+      head = newNode
+      tail = newNode
+      return
+    }
+    
+    newNode.previous = tailNode
+    tailNode.next = newNode
+    tail = newNode
+  }
+  
+  public func remove(_ node: DNode<T>) -> T {
+    let prev = node.previous
+    let next = node.next
+    
+    if let prev = prev {
+      prev.next = next
+    } else {
+      head = next
+    }
+    
+    next?.previous = prev
+    
+    if next == nil {
+      tail = prev
+    }
+    
+    node.previous = nil
+    node.next = nil
+    
+    return node.value
+  }
+}
+
+extension DoublyLinkedList: CustomStringConvertible {
+  
+  public var description: String {
+    var string = ""
+    var current = head
+    while let node = current {
+      string.append("\(node.value) -> ")
+      current = node.next
+    }
+    return string + "end"
+  }
+}
+
+public class LinkedListIterator<T>: IteratorProtocol {
+  
+  private var current: DNode<T>?
+  
+  init(node: DNode<T>?) {
+    current = node
+  }
+  
+  public func next() -> DNode<T>? {
+    defer { current = current?.next }
+    return current
+  }
+}
+
+extension DoublyLinkedList: Sequence {
+  
+  public func makeIterator() -> LinkedListIterator<T> {
+    LinkedListIterator(node: head)
+  }
+}
+
 public class DNode<T> {
   
   public var value: T
