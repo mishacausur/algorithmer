d2f9c251b9090b9564afb6b64e8bb480e367e4ae
Misha Causur <mishacausur@mail.ru>
2022-08-04 23:13:02 +0500

    small refactoring

--- algorithmer/Queue/queue_ring_buffer.swift
+++ algorithmer/Queue/queue_ring_buffer.swift
@@ -22,11 +22,11 @@ public struct QueueRBuffer<T>: Queue {
     public var peek: T? {
         buffer.first
     }
-    
+    /// O(1)
     public mutating func enqueue(_ element: T) -> Bool {
         buffer.write(element)
     }
-    
+    /// O(1)
     public mutating func dequeue() -> T? {
         buffer.read()
     }
@@ -35,46 +35,6 @@ public struct QueueRBuffer<T>: Queue {
 extension QueueRBuffer: CustomStringConvertible {
     public var description: String {
         String(describing: buffer)
-    }
-}
-
-public struct RingBuffer<T> {
-    
-    private var array: [T?]
-    private var indexRead = 0
-    private var indexWrite = 0
-    private var availableReadingSpace: Int {
-        return indexWrite - indexRead
-    }
-    private var availableWritingSpace: Int {
-        return array.count - indexRead
-    }
-    public var first: T? {
-        array[indexRead]
-    }
-    public var isEmpty: Bool {
-        availableReadingSpace == 0
-    }
-    public var isFull: Bool {
-        availableWritingSpace == 0
-    }
-    
-    public init(count: Int) {
-        array = [T?](repeating: nil, count: count)
-    }
-    
-    public mutating func write(_ element: T) -> Bool {
-        guard !isFull else { return false }
-        array[indexWrite % array.count] = element
-        indexWrite += 1
-        return true
-    }
-    
-    public mutating func read() -> T? {
-        guard !isEmpty else { return nil }
-        let element = array[indexRead % array.count]
-        indexRead += 1
-        return element
     }
 }
 
