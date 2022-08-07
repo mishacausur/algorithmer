46b3aa7cc01346bb49b1cb578a8903b1a6d83bc2
Misha Causur <mishacausur@mail.ru>
2022-08-07 23:46:46 +0500

    queue stack in progress

--- algorithmer/Queue/queue_double_stack.swift
+++ algorithmer/Queue/queue_double_stack.swift
@@ -8,7 +8,17 @@
 import Foundation
 
 public struct QueueStack<T>: Queue {
+    
     private var leftStack: [T] = []
     private var rightStack: [T] = []
+    
+    public var isEmpty: Bool {
+        leftStack.isEmpty && rightStack.isEmpty
+    }
+    
+    public var peek: T? {
+        !leftStack.isEmpty ? leftStack.last : rightStack.first
+    }
+    
     public init() {}
 }
