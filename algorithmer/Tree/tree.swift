17c93f31ae9af5f769ca76d269c40eb0adc87b17
Misha Causur <mishacausur@mail.ru>
2022-08-10 00:14:19 +0500

    for depth first method is implemented

--- algorithmer/Tree/tree.swift
+++ algorithmer/Tree/tree.swift
@@ -20,6 +20,15 @@ public class TreeNode<T> {
     }
 }
 
+extension TreeNode {
+    public func depthFirst(visit: (TreeNode) -> Void) {
+        visit(self)
+        children.forEach {
+            $0.depthFirst(visit: visit)
+        }
+    }
+}
+
 extension Example {
     func treeExample() {
         let beverages = TreeNode("BEVERAGES")
@@ -28,5 +37,50 @@ extension Example {
         
         beverages.add(hot)
         beverages.add(cold)
+    }
+    
+    func treeExample2() {
+        let tree = makeBeverageTree()
+        tree.depthFirst { print($0.value) }
+    }
+    
+    func makeBeverageTree() -> TreeNode<String> {
+      let tree = TreeNode("Beverages")
+
+      let hot = TreeNode("hot")
+      let cold = TreeNode("cold")
+
+      let tea = TreeNode("tea")
+      let coffee = TreeNode("coffee")
+      let chocolate = TreeNode("cocoa")
+
+      let blackTea = TreeNode("black")
+      let greenTea = TreeNode("green")
+      let chaiTea = TreeNode("chai")
+
+      let soda = TreeNode("soda")
+      let milk = TreeNode("milk")
+
+      let gingerAle = TreeNode("ginger ale")
+      let bitterLemon = TreeNode("bitter lemon")
+
+      tree.add(hot)
+      tree.add(cold)
+
+      hot.add(tea)
+      hot.add(coffee)
+      hot.add(chocolate)
+
+      cold.add(soda)
+      cold.add(milk)
+
+      tea.add(blackTea)
+      tea.add(greenTea)
+      tea.add(chaiTea)
+
+      soda.add(gingerAle)
+      soda.add(bitterLemon)
+
+      return tree
     }
 }
