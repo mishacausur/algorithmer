09d3ead10b806c815c65078f1a7ffd7715148c32
Misha Causur <mishacausur@mail.ru>
2022-08-09 23:58:57 +0500

    tree node is implemented

---
+++ algorithmer/Tree/tree.swift
@@ -0,0 +1,23 @@
+//
+//  tree.swift
+//  algorithmer
+//
+//  Created by Misha Causur on 09.08.2022.
+//
+
+import Foundation
+
+public class TreeNode<T> {
+    public var value: T
+    public var children: [TreeNode] = []
+    
+    public init(_ value: T) {
+        self.value = value
+    }
+    
+    public func add(_ child: TreeNode) {
+        children.append(child)
+    }
+}
+
+
