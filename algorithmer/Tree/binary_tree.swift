9adf576fece8da2658a91a2c4df445af9c616a3a
Misha Causur <mishacausur@mail.ru>
2022-08-23 23:17:08 +0500

    binary node example added

--- algorithmer/Tree/binary_tree.swift
+++ algorithmer/Tree/binary_tree.swift
@@ -16,3 +16,24 @@ public class BinaryNode<Element> {
         self.value = value
     }
 }
+
+extension Example {
+    func binaryNodeExample() {
+        var tree: BinaryNode<Int> = {
+          let zero = BinaryNode(value: 0)
+          let one = BinaryNode(value: 1)
+          let five = BinaryNode(value: 5)
+          let seven = BinaryNode(value: 7)
+          let eight = BinaryNode(value: 8)
+          let nine = BinaryNode(value: 9)
+
+          seven.left = one
+          one.left = zero
+          one.right = five
+          seven.right = nine
+          nine.left = eight
+
+          return seven
+        }()
+    }
+}
