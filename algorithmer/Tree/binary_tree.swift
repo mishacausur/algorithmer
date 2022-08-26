cec11e1685bacd6224af80bdaec1ee0536e8d666
Misha Causur <mishacausur@mail.ru>
2022-08-26 23:55:34 +0500

    binary node description

--- algorithmer/Tree/binary_tree.swift
+++ algorithmer/Tree/binary_tree.swift
@@ -17,6 +17,30 @@ public class BinaryNode<Element> {
     }
 }
 
+extension BinaryNode: CustomStringConvertible {
+    
+    public var description: String {
+        diagram(for: self)
+    }
+    
+    private func diagram(for node: BinaryNode?,
+                         _ top: String = "",
+                         _ root: String = "",
+                         _ bottom: String = "") -> String {
+        guard let node = node else {
+            return root + "nil\n"
+        }
+        if node.leftChild == nil && node.rightChild == nil {
+            return root + "\(node.value)\n"
+        }
+        return diagram(for: node.rightChild,
+                       top + " ", top + "┌──", top + "│ ")
+        + root + "\(node.value)\n"
+        + diagram(for: node.leftChild,
+                  bottom + "│ ", bottom + "└──", bottom + " ")
+    }
+}
+
 extension Example {
     func binaryNodeExample() {
         var tree: BinaryNode<Int> = {
