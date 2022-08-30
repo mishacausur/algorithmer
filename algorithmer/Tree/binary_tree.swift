5fdd980efcb3dcf6deabd007cfedd8384d1b58be
Misha Causur <mishacausur@mail.ru>
2022-08-30 23:43:48 +0500

    example 2 added

--- algorithmer/Tree/binary_tree.swift
+++ algorithmer/Tree/binary_tree.swift
@@ -68,4 +68,26 @@ extension Example {
           return seven
         }()
     }
+    
+   func binaryNodeExample1() {
+       
+       var tree: BinaryNode<Int> = {
+         let zero = BinaryNode(value: 0)
+         let one = BinaryNode(value: 1)
+         let five = BinaryNode(value: 5)
+         let seven = BinaryNode(value: 7)
+         let eight = BinaryNode(value: 8)
+         let nine = BinaryNode(value: 9)
+
+         seven.left = one
+         one.left = zero
+         one.right = five
+         seven.right = nine
+         nine.left = eight
+
+      return seven
+       }()
+       
+       tree.traverseInOrder { print($0) }
+   }
 }
