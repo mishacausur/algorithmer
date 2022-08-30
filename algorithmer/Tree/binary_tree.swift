db1a45b98776834bbadb3dc24ec0754733fe7a50
Misha Causur <mishacausur@mail.ru>
2022-08-30 23:40:05 +0500

    example fixed

--- algorithmer/Tree/binary_tree.swift
+++ algorithmer/Tree/binary_tree.swift
@@ -41,23 +41,23 @@ extension BinaryNode: CustomStringConvertible {
     }
 }
 
-//extension Example {
-//    func binaryNodeExample() {
-//        var tree: BinaryNode<Int> = {
-//          let zero = BinaryNode(value: 0)
-//          let one = BinaryNode(value: 1)
-//          let five = BinaryNode(value: 5)
-//          let seven = BinaryNode(value: 7)
-//          let eight = BinaryNode(value: 8)
-//          let nine = BinaryNode(value: 9)
-//
-//          seven.left = one
-//          one.left = zero
-//          one.right = five
-//          seven.right = nine
-//          nine.left = eight
-//
-//          return seven
-//        }()
-//    }
-//}
+extension Example {
+    func binaryNodeExample() {
+        var tree: BinaryNode<Int> = {
+          let zero = BinaryNode(value: 0)
+          let one = BinaryNode(value: 1)
+          let five = BinaryNode(value: 5)
+          let seven = BinaryNode(value: 7)
+          let eight = BinaryNode(value: 8)
+          let nine = BinaryNode(value: 9)
+
+          seven.left = one
+          one.left = zero
+          one.right = five
+          seven.right = nine
+          nine.left = eight
+
+          return seven
+        }()
+    }
+}
